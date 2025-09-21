e36ea7740e723a415cfd7e68cec94fb9c003c0c9
Selene1203 <sliqman42@gmail.com>
2025-09-21 20:16:53 +0200

    Update Grain counting program.cpp

--- Grain counting program.cpp
+++ Grain counting program.cpp
@@ -17,3 +17,33 @@ void cattleWealth() {
     int cattle;
     cout << "Enter number of cattle: ";
     cin >> cattle;
+int makopokopo = litres / 20;
+    int remainder = litres % 20;
+
+    cout << "Makopokopo (20L): " << makopokopo << endl;
+    cout << "Remaining litres: " << remainder << endl;
+}
+
+void cattleWealth() {
+    int cattle;
+    cout << "Enter number of cattle: ";
+    cin >> cattle;
+
+    if (cattle < 8) {
+        cout << "Not enough for bohali" << endl;
+    } else if (cattle < 30) {
+        cout << "Small herd" << endl;
+    } else {
+        cout << "Wealthy cattle owner" << endl;
+    }
+}
+
+int main() {
+    cout << "--- Grain Counting (Makopokopo) ---" << endl;
+    grainCounting();
+
+    cout << "\n--- Cattle Wealth (Mahali) ---" << endl;
+    cattleWealth();
+
+    return 0;
+}
